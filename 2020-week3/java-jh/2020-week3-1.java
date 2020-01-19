5270831887e4d2366d5617745d6cc50e9fc730b9
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-01-20 00:50:58 +0900

    [Fix] 2020-week3-1 오타 수정

--- 2020-week3/java-jh/2020-week3-1.java
+++ 2020-week3/java-jh/2020-week3-1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Queue<Integer> q = new LinkedList<Integer>();
