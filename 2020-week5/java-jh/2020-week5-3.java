3b273040bf8fcd5117dbf28526b2def27cd15ace
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-02-10 19:01:12 +0900

    [Add] 2020-week5-3

---
+++ 2020-week5/java-jh/2020-week5-3.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[][] roads = new int[n][n];
+        StringTokenizer st = new StringTokenizer("");
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                roads[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (roads[j][i] == 1 && roads[i][k] == 1)
+                        roads[j][k] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write(roads[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
