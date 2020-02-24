0991fe80542e8ddac0dd1804704dbb7863fdc9f3
JaeHyeonKim19 <jaehyeonkim19@gmail.com>
2020-02-24 19:15:18 +0900

    Fix: 2020-week6-1 unsolved

--- 2020-week6/java-jh/2020-week6-1.java
+++ 2020-week6/java-jh/2020-week6-1.java
@@ -83,46 +83,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int [][] dp;
-	
-	static void isPalindrome(int [] nums, int from, int to) {
-		int i = 0;
-		while(from + i < to - i) {
-			if(nums[from + i] != nums[to - i]) return;
-			i++;
-		}
-		i = 0;
-		while(from + i <= to - i) {
-			dp[from + i][to - i] = 1;
-			i++;
-		}
-		return;
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		dp = new int[n + 1][n + 1];
-		int [] nums = new int[n + 1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= n; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		for(int i = n; i >= 1; i--) {
-			for(int j = n; j >= 1; j--) {
-				if(dp[i][j] != 1) isPalindrome(nums, i, j);
-			}
-		}
-		int m = Integer.parseInt(br.readLine());
-		int from;
-		int to;
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			from = Integer.parseInt(st.nextToken());
-			to = Integer.parseInt(st.nextToken());
-			bw.write(dp[from][to] + "\n");
-		}
-		bw.flush();
-	}
+    static int[][] dp;
+
+    static void isPalindrome(int[] nums, int from, int to) {
+        int i = 0;
+        while (from + i < to - i) {
+            if (nums[from + i] != nums[to - i])
+                return;
+            i++;
+        }
+        i = 0;
+        while (from + i <= to - i) {
+            dp[from + i][to - i] = 1;
+            i++;
+        }
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1][n + 1];
+        int[] nums = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j >= 1; j--) {
+                if (dp[i][j] != 1)
+                    isPalindrome(nums, i, j);
+            }
+        }
+        int m = Integer.parseInt(br.readLine());
+        int from;
+        int to;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            from = Integer.parseInt(st.nextToken());
+            to = Integer.parseInt(st.nextToken());
+            bw.write(dp[from][to] + "\n");
+        }
+        bw.flush();
+    }
 }
