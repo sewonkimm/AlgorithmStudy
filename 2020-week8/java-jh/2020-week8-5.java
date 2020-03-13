284805d465d37c8eee1fabb00262e6f0c4e30807
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-03-14 01:17:31 +0900

    Fix: Fix problems number

--- 2020-week8/java-jh/2020-week8-5.java
+++ 2020-week8/java-jh/2020-week8-5.java
@@ -2,39 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int count = 0;
+    static int[] seats;
+    static boolean[] isReserved;
+
+    static void rec(int waiting, int index) {
+        if (waiting == 0) {
+            count++;
+            return;
+        }
+        for (int i = index + 2; i <= seats.length - (waiting * 2 - 1); i++) {
+            rec(waiting - 1, i);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        long[][] nums = new long[n][n];
-        if (n == 1) {
-            bw.write(1 + " ");
-            bw.flush();
-            return;
-        } else if (n == 2) {
-            bw.write(1 + " " + "\n");
-            bw.write(1 + " " + 1 + " ");
-            bw.flush();
-            return;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        seats = new int[n];
+        isReserved = new boolean[n];
+        for (int i = 0; i <= n - (k * 2 - 1); i++) {
+            rec(k - 1, i);
         }
-        nums[0][0] = 1;
-        nums[1][0] = 1;
-        nums[1][1] = 1;
-        for (int i = 2; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    nums[i][j] = 1;
-                    continue;
-                }
-                nums[i][j] = nums[i - 1][j - 1] + nums[i - 1][j];
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                bw.write(nums[i][j] + " ");
-            }
-            bw.write("\n");
-        }
-        bw.flush();
+        System.out.print(count);
     }
 }
