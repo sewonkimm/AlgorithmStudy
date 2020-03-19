74e02e8998ea3f848bba1599626f252ca5508c30
JaeHyeonKim19 <jaehyeonkim19@gmail.com>
2020-03-20 00:44:40 +0900

    Add: 2020-week9-5

---
+++ 2020-week9/java-jh/2020-week9-5.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw;
+	static int[] nums;
+
+	static void rec(int n, int count) throws IOException {
+		if (count == nums.length) {
+			for (int i = 0; i < nums.length; i++) {
+				bw.write(nums[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			nums[count] = i;
+			rec(n, count + 1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		nums = new int[m];
+		rec(n, 0);
+		bw.flush();
+	}
+}
